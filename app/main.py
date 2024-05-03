c8a948328a3f515b43cafbc2be51d8f594718930
Bello Ango <angobello0@gmail.com>
2024-05-03 22:59:05 +0100

    app: create book module

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
 from app.common.dependencies import get_db
-from app.example_module.apis import router as example_router
+from app.book.apis import router as book_router
 
 
 # Lifespan (startup, shutdown)
@@ -62,4 +62,4 @@ async def health_check(_=Depends(get_db)):
 
 
 # Routers
-app.include_router(example_router, prefix="/example", tags=["Example Docs"])
+app.include_router(book_router, prefix="/books", tags=["books"])
